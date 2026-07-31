345126acac0d42a3e11734392a94e37c5e2ebe18
eddiesteers777 <eddiesteers777@gmail.com>
2026-07-31 09:17:04 -0500

    Create loadHeader.js

---
+++ js/loadHeader.js
@@ -0,0 +1,6 @@
+fetch("components/header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("header").innerHTML = data;
+    })
+    .catch(error => console.error("Error loading header:", error));
